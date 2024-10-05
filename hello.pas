0dc70c113af08fad8766beed90d814c03ce8f9a4
Владислав Скрипников <roadtosad@gmail.com>
2024-10-05 05:45:59 +0300

    Initial commit

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  print('Hello world!')
+  var N := ReadInteger('Введите час от 0 до 23:');
+  Assert(N in 0..23);
+  case N of
+    4..10: print('Доброе утро, мир!');
+    11..16: print('Добрый день, мир!');
+    17..22: print('Доброй вечер, мир!');
+    else: print('Доброй ночи, мир!');
+  end;
 end.
