b3089b9903392a75e4c4dfeca31f364c05f4d784
Владислав Скрипников <roadtosad@gmail.com>
2024-10-05 05:54:34 +0300

    Новая программа

---
+++ time.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var N := ReadInteger('Введите год:');
+  Assert(N>0);
+  if (N mod 4 = 0) and (N mod 100 = 0) and (N mod 400 <> 0) then
+    print('Год високосный')
+  else
+    print('Год не високосный')
+end.
